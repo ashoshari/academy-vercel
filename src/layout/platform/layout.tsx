cd876a3d944977f49b2c673ae826953d8fc17d76
Mahmoud elqenawey <mahmoudelqenawey1@gmail.com>
2025-09-07 23:50:47 +0300

    update layout

--- src/layout/platform/layout.tsx
+++ src/layout/platform/layout.tsx
@@ -12,36 +12,42 @@ const isProduction = import.meta.env.MODE === "production"; // ✅ check env
 
 if (isProduction) {
   document.onkeydown = function (e: any) {
-    if (e.keyCode == 123) {
-      return false;
-    }
     if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
       return false;
     }
+
     if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
       return false;
     }
+
     if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
       return false;
     }
+
     // if (e.ctrlKey && e.keyCode == "C".charCodeAt(0)) {
     //   return false;
     // }
+
     if (e.ctrlKey && e.keyCode == "X".charCodeAt(0)) {
       return false;
     }
+
     if (e.ctrlKey && e.keyCode == "Y".charCodeAt(0)) {
       return false;
     }
+
     if (e.ctrlKey && e.keyCode == "Z".charCodeAt(0)) {
       return false;
     }
+
     // if (e.ctrlKey && e.keyCode == "V".charCodeAt(0)) {
     //   return false;
     // }
+
     if (e.keyCode == 67 && e.shiftKey && (e.ctrlKey || e.metaKey)) {
       return false;
     }
+
     if (
       e.keyCode == "J".charCodeAt(0) &&
       e.altKey &&
@@ -49,6 +55,7 @@ if (isProduction) {
     ) {
       return false;
     }
+
     if (
       e.keyCode == "I".charCodeAt(0) &&
       e.altKey &&
@@ -56,27 +63,34 @@ if (isProduction) {
     ) {
       return false;
     }
-    if (
-      (e.keyCode == "V".charCodeAt(0) && e.metaKey) ||
-      (e.metaKey && e.altKey)
-    ) {
-      return false;
-    }
+
+    // if (
+    //   (e.keyCode == "V".charCodeAt(0) && e.metaKey) ||
+    //   (e.metaKey && e.altKey)
+    // ) {
+    //   return false;
+    // }
+
     if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
       return false;
     }
+
     if (e.ctrlKey && e.keyCode == "S".charCodeAt(0)) {
       return false;
     }
+
     if (e.ctrlKey && e.keyCode == "H".charCodeAt(0)) {
       return false;
     }
+
     if (e.ctrlKey && e.keyCode == "A".charCodeAt(0)) {
       return false;
     }
+
     if (e.ctrlKey && e.keyCode == "F".charCodeAt(0)) {
       return false;
     }
+
     if (e.ctrlKey && e.keyCode == "E".charCodeAt(0)) {
       return false;
     }
