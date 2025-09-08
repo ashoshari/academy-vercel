e3790de17b1344e531656a718a86478224467bd5
Mohammed Wael <mohammedsama2017@gmail.com>
2025-09-08 19:42:35 +0300

    fixing some bugs in register and in core Exam

--- src/components/platform/exams/coreExam.tsx
+++ src/components/platform/exams/coreExam.tsx
@@ -446,7 +446,7 @@ const CoreExam = ({
                         (a: any) => a?.question_id === question?.id
                       );
                       const isCorrect = matchedAnswer?.is_correct;
-                      const correctAnswerId = matchedAnswer?.correct_answer_id;
+                      // const correctAnswerId = matchedAnswer?.correct_answer_id;
                       const userAnswerId = matchedAnswer?.user_answer_id;
                       const currentAnswer = answers?.[questionIndex];
                       return (
