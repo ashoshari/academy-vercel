2888d1f43068aafddb293270c472f0690099ce90
dev-supervision <dev.visionjo@gmail.com>
2026-04-13 20:32:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
